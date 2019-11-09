9874e4ced29d8ff6406f0716e13a143f78177a93
leopard <caokai@iwown.com>
2019-11-09 17:17:04 +0800

    1.2.1

--- podTrunk.sh
+++ podTrunk.sh
@@ -8,7 +8,7 @@ fi
 
 VERSION=$1
 
-cd /Users/scow/GitHub/IVBaseKit/ 
+cd ~/GitHub/IVBaseKit/ 
 sed -i "" "s/\([0-9]\)\.\([0-9]\)\.\([0-9]\)/${VERSION}/g" IVBaseKit.podspec
 git add .
 git commit -am "${VERSION}" 
